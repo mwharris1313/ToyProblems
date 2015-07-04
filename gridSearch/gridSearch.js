9d50b010e79688f9eec0348b4b67059379cb5934
Michael Harris <mwharris333@gmail.com>
2015-07-04 13:15:34 -0700

    Add problem statement for gridSearch

---
+++ gridSearch/gridSearch.js
@@ -0,0 +1,18 @@
+/*
+Given a 2D array made up of X's and O's, find the maximum number of non-overlapping occurances of an N x N pattern
+
+INPUT ARRAY:
+O X O X O 
+X O X O X 
+O X O X O 
+X O X O O
+O X O O O
+
+INPUT PATTERN:
+O X O
+X O X
+O X O
+
+OUTPUT:
+2
+*/
