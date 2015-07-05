223fff1dacb27fb396cb89670155526122c75cd9
Michael Harris <mwharris333@gmail.com>
2015-07-04 23:04:08 -0700

    Add problem statement for graphPaths.js

---
+++ graphPaths/graphPaths.js
@@ -0,0 +1,11 @@
+/*
+Given a graph of n inter-connected nodes, with node 1 and node 2 being nodes within the graph,
+Find all the non-overlapping paths from node 1 to node 2.
+
+EC: find the shortest path from node 1 to node 2.
+*/
+
+var graphPaths = function (node1, node2) {
+
+  return result;
+};
