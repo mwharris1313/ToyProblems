442d0e876eb88a75ed8edab757c9d03fb98d03e8
Michael Harris <mwharris333@gmail.com>
2015-07-04 22:51:23 -0700

    Add problem statement for treePaths.js

---
+++ treePaths/treePaths.js
@@ -0,0 +1,9 @@
+/*
+Given a tree, find all the paths starting at the root that lead to a descendent containing a specified value
+
+*/
+
+var treePaths = function (tree, value) {
+
+  return result;
+};
